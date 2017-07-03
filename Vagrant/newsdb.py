93c62cc591842eb6e6e9bcb2a0efe35375879ed8
anujaverma11 <anuja.verma@gmail.com>
2017-07-03 10:01:49 -0700

    Change method definations to remove repetition

--- Vagrant/newsdb.py
+++ Vagrant/newsdb.py
@@ -8,12 +8,22 @@ import bleach
 DBNAME = "news"
 
 
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("<error message>")
+
+
 def get_popular_articles():
     """ 1. What are the most popular three articles of all time?"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select * from popular_articles limit 3")
-    articles = c.fetchall()
+    db, cursor = connect()
+
+    query = "select * from popular_articles limit 3"
+    cursor.execute(query)
+    articles = cursor.fetchall()
     # print (articles)
     for i in range(len(articles)):
         print("%s - %d views" % (articles[i][0], articles[i][1]))
@@ -22,15 +32,13 @@ def get_popular_articles():
 
 def get_popular_authors():
     """ 2. Who are the most popular article authors of all time?"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    author_query = """
-                  select name, popularity
-                  from authors join popular_article_authors
-                  on popular_article_authors.author = authors.id;
-                  """
-    c.execute(author_query)
-    authors = c.fetchall()
+    query = """
+              select name, popularity
+              from authors join popular_article_authors
+              on popular_article_authors.author = authors.id;
+              """
+    cursor.execute(query)
+    authors = cursor.fetchall()
     # print (authors)
     for i in range(len(authors)):
         print("%s - %d views" % (authors[i][0], authors[i][1]))
@@ -40,10 +48,11 @@ def get_popular_authors():
 
 def get_error_details():
     """ 3. On which days did more than 1% of requests lead to errors?"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select * from request_errors;")
-    req_errors = c.fetchall()
+    db, cursor = connect()
+
+    query = "select * from request_errors;"
+    cursor.execute(query)
+    req_errors = cursor.fetchall()
     # print (req_errors)
 
     for i in range(len(req_errors)):
--- Vagrantfile
+++ Vagrantfile
@@ -3,13 +3,13 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "bento/ubuntu-16.04-i386"
-  config.vm.network "forwarded_port", guest: 7000, host: 7000, host_ip: "127.0.0.1"
-  config.vm.network "forwarded_port", guest: 7070, host: 7070, host_ip: "127.0.0.1"
-  config.vm.network "forwarded_port", guest: 7000, host: 7000, host_ip: "127.0.0.1"
+  config.vm.network "forwarded_port", guest: 4000, host: 4000, host_ip: "127.0.0.1"
+  config.vm.network "forwarded_port", guest: 4040, host: 4040, host_ip: "127.0.0.1"
+  config.vm.network "forwarded_port", guest: 4000, host: 4000, host_ip: "127.0.0.1"
 
   # Work around disconnected virtual network cable.
   config.vm.provider "virtualbox" do |vb|
-    vb.customize ["modifyvm", :id, "--cableconnected1", "on"]cd
+    vb.customize ["modifyvm", :id, "--cableconnected1", "on"]
   end
 
   config.vm.provision "shell", inline: <<-SHELL
